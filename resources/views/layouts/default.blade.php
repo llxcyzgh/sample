aed03de67baeba630d9114fb04a48c0e8a00377d
zhu <1559296632@qq.com>
2018-08-21 13:38:32 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,15 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>@yield('title','Sample App')</title>
+    <title>@yield('title','Sample App')--for zhuli</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+@include('layouts._header')
+
+<div class="container">
     @yield('content')
+    @include('layouts._footer')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.default')
+
 @section('title','home')
+
 @section('content')
-    <h1>主页 hi zhuli welcome to the world</h1>
+    <div class="jumbotron">
+        <h1>hi zhuli</h1>
+        <p class="lead"> welcome to the world <a href="#">这是第一个laraver程序</a> can you see</p>
+        <p>
+            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
+
 @stop
