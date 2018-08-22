e3d1451e69c78ef268e8b3c9fa674c790764431c
zhu <1559296632@qq.com>
2018-08-22 08:30:15 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
+//use App\Http\Requests;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -17,4 +17,26 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show',[$user]);
+//        return redirect()->route('users.show',[$user->id]);
+    }
+
+
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,51 @@
 @section('title','注册')
 
 @section('content')
-    <h1>注册表单</h1>
+    <div class="col-md-offset-2 col-md-8">
+        <div class="panel panel-default">
+            {{--标题--}}
+            <div class="panel-heading">
+                <h1>注册表单</h1>
+            </div>
+            {{--表单--}}
+            <div class="panel-body">
+                <form action="{{route('users.store')}}" method="post">
+                    {{--引入错误信息--}}
+                    @include('shared._errors')
+
+                    {{csrf_field()}}
+
+                    {{--名称--}}
+                    <div class="form-group">
+                        <label for="name">名称</label>
+                        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
+                    </div>
+
+                    {{--邮箱--}}
+                    <div class="form-group">
+                        <label for="email">邮箱</label>
+                        <input type="text" name="email" id="email" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    {{--密码--}}
+                    <div class="form-group">
+                        <label for="password">密码</label>
+                        <input type="password" name="password" id="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    {{--确认密码--}}
+                    <div class="form-group">
+                        <label for="password_confirmation">确认密码</label>
+                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    {{--提交按钮--}}
+                    <button type="submit" class="btn btn-primary">注册</button>
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+    
 @stop
