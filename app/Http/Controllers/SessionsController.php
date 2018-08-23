36dc06101520a539e2935ea23ccd1d68803f6b8e
zhu <1559296632@qq.com>
2018-08-23 13:22:10 +0000

    Finish login and logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials,$request->has('remember'))){
             // 登陆成功的相关操作
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
@@ -29,7 +29,12 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back();
         }
+    }
 
-
+    public function destory()
+    {
+        Auth::logout();
+        session()->flash('success','您已成功退出');
+        return redirect('login');
     }
 }
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -24,8 +24,15 @@
                     {{--登陆密码--}}
                     <div class="form-group">
                         <label for="password">密码</label>
-                        <input type="password" name="password" id="password" class="form-control" value="{{ old('password') }}">
+                        <input type="password" name="password" id="password" class="form-control"
+                               value="{{ old('password') }}">
                     </div>
+
+                    {{--记住我--}}
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="remember">记住我</label>
+                    </div>
+
 
                     {{--提交按钮--}}
                     <button type="submit" class="btn btn-primary">登陆</button>
@@ -35,5 +42,5 @@
             </div>
         </div>
     </div>
-    
+
 @stop
