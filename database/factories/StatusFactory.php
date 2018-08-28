09e345065d128c8d9fb5af157d6567d7bcf261c4
vagrant <vagrant@homestead>
2018-08-28 00:59:34 +0000

    still composer require-dev

--- database/factories/StatusFactory.php
+++ database/factories/StatusFactory.php
@@ -3,7 +3,7 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Status::class, function (Faker $faker) {
-    $date_time = $faker->date . '' . $faker->time;
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
         'content'    => $faker->text,
         'created_at' => $date_time,
